2616035742720b9537bca30e9901d63b292ccc64
joyymungaii <joywanjirum4@gmail.com>
2026-01-23 07:13:24 +0300

    Initial C program

--- main.c
+++ main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
 int main(){
-    printf("Hello, Joy!!");
+    printf("Hello, Joy!!\n");
+    printf("pacman -Syu\n");
+    printf("pacman -S mingw-w64-x86_64-gcc\n");
+    printf("gcc --version\n");
+    printf("open MSYS2 MinGW64\n");
+    printf("cd /c/Users/HP/Desktop/Joy/Robotics/Files\n");
+    printf("ls\n");
+    printf("gcc main.c -o main\n");
+    printf("./main\n");
     return 0;
 }
 

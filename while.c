5ab65c8ade96dfb8a922deffa6bba228e815ec49
joyymungaii <joywanjirum4@gmail.com>
2026-01-28 07:23:37 +0300

    reverse loop

--- while.c
+++ while.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    int i = 10;
-    while (i >= 1) {
+    int i = 1;
+    while (i <= 10) {
         printf("%d\n", i);
-        i--;
+        i++;
     }
     return 0;
 }

c1515bc502b9993befa2420e7806850dbb649468
joyymungaii <joywanjirum4@gmail.com>
2026-03-26 06:35:49 +0300

    duplicate

--- mine.c
+++ mine.c
@@ -30,3 +30,36 @@ int main() {
     
     return 0;
 }
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int choice;
+    int result;
+    
+    srand(time(0));
+    
+    printf("=== Coin Tossing Game ===\n");
+    
+    while (1) {
+        printf("\nPress 1 to toss the coin (or any other key to exit): ");
+        scanf("%d", &choice);
+        
+        if (choice != 1) {
+            printf("Thanks for playing!\n");
+            break;
+        }
+        
+        result = rand() % 2;
+        
+        if (result == 0) {
+            printf("Result: HEADS\n");
+        } else {
+            printf("Result: TAILS\n");
+        }
+    }
+    
+    return 0;
+}
